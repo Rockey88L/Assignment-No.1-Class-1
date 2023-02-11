85531a404f4807c4f32d052bcbbc0f9a6abcf566
Rockey88L <abdullahrauf88@yahoo.com>
2023-02-11 23:59:21 +0500

    Ex 11

---
+++ ex11.ts
@@ -0,0 +1,8 @@
+//No.11
+//Names: Store the names of a few of your friends in a array called names.
+//  Print each person’s name by accessing each element in the list, one at a time.
+
+const names = ["Rex", "Cats", "Rekty", "Chuchu", "ET", "Smokey?"];
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
