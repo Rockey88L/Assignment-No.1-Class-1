1554c4f7caf52679402bfca33a263e7ebce8fa30
Rockey88L <abdullahrauf88@yahoo.com>
2023-02-13 16:18:59 +0500

    ex15

---
+++ ex25.ts
@@ -0,0 +1,13 @@
+// No.25
+// Alien Colors #1: Imagine an alien was just shot down in a game.
+//  Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+let alien_color = "green";
+//passed If test
+if (alien_color == "green") {
+  console.log("Congras! You have earned 5 Points!");
+}
+//Failed if test
+if (alien_color == "yellow") {
+  console.log("");
+}
