2ac578104e3c6b3df038d41ad8c47f6077c96fd1
Rockey88L <abdullahrauf88@yahoo.com>
2023-02-13 21:55:32 +0500

    EX36

---
+++ ex36.ts
@@ -0,0 +1,9 @@
+//No.36
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+
+function make_shirt(size, message){
+
+    return console.log("The size of the shirt is:" + size + ", and it has a " + message + " logo printed on it");
+}
+ make_shirt(23, "Berserk"); //calling the function with parameters
+
