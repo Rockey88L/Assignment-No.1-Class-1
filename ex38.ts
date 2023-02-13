aa7c5b879b88e663a1069bf3d7b878a5979c3e35
Rockey88L <abdullahrauf88@yahoo.com>
2023-02-13 22:14:53 +0500

    Ex38

---
+++ ex38.ts
@@ -0,0 +1,10 @@
+//No.38
+//Cities: Write a function called describe_city() that accepts the name of a city and its country.
+//The function should print a simple sentence, such as Karachi is in Pakistan.
+
+function describe_city(name, country = "Pakistan") {
+  return console.log(name + " is in " + country);
+}
+describe_city("Karachi"); // Message with default;
+describe_city("Islamabad"); // Different message;
+describe_city("Tokyo", "Japan"); // Is not in default country
