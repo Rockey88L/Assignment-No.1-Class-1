7add17fff4f9b0cef211181ac956d19defeadb82
Rockey88L <abdullahrauf88@yahoo.com>
2023-02-13 16:58:11 +0500

    ex29

---
+++ ex29.ts
@@ -0,0 +1,21 @@
+//No.29
+// Favorite Fruit: Make a array of your favorite fruits,
+//  and then write a series of independent if statements that check for certain fruits in your array.
+
+let favorite_fruits = ["ananas", "blueberry", "cherry"];
+
+if (favorite_fruits.includes("ananas")) {
+  console.log("You really like Ananas!");
+}
+if (favorite_fruits.includes("blueberry")) {
+  console.log("You really like Blueberries!");
+}
+if (favorite_fruits.includes("cherry")) {
+  console.log("You really like Cherries!");
+}
+if (favorite_fruits.includes("oranges")) {
+  console.log("You really like Oranges!");
+}
+if (favorite_fruits.includes("Peach")) {
+  console.log("You really like Peach!");
+}
