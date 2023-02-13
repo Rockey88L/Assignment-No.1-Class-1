3a839f8105c5dfd2f898cf00450f17e7e77790e6
Rockey88L <abdullahrauf88@yahoo.com>
2023-02-13 23:44:51 +0500

    Ex41

---
+++ ex41.ts
@@ -0,0 +1,15 @@
+//No.41
+// Magicians: Make a array of magician’s names.
+//  Pass the array to a function called show_magicians(),
+//  which prints the name of each magician in the array.
+
+let magicianNames = ["Houdini", "My Will", "Money", "Nawaz Shareef", "Zardari"];
+
+function show_magicians(magicianNames){
+
+    for(let i = 0; i < magicianNames.length; i++){
+            console.log(magicianNames[i]);
+    }
+    return
+}
+show_magicians(magicianNames);
