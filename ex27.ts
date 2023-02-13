f7c8274ae8dd1ffefb6432b14025edf14eb6fe1c
Rockey88L <abdullahrauf88@yahoo.com>
2023-02-13 16:32:45 +0500

    ex27

---
+++ ex27.ts
@@ -0,0 +1,33 @@
+//No.27
+// Alien Colors #3:
+// Turn your if-else chain from Exercise 5-4 into an if-else chain.
+
+//green color version
+let alien_colorG = "green";
+if (alien_colorG == "green") {
+  console.log("You have earned 5 Points!");
+} else if (alien_colorG == "yellow") {
+  console.log("You have earned 10 Points!");
+} else if (alien_colorG == "red") {
+  console.log("You have earned 15 Points!");
+}
+
+//yellow color version
+let alien_colorY = "yellow";
+if (alien_colorY == "green") {
+  console.log("You have earned 5 Points!");
+} else if (alien_colorY == "yellow") {
+  console.log("You have earned 10 Points!");
+} else if (alien_colorY == "red") {
+  console.log("You have earned 15 Points!");
+}
+
+//red color version
+let alien_colorR = "red";
+if (alien_colorR == "green") {
+  console.log("You have earned 5 Points!");
+} else if (alien_colorR == "yellow") {
+  console.log("You have earned 10 Points!");
+} else if (alien_colorR == "red") {
+  console.log("You have earned 15 Points!");
+}
