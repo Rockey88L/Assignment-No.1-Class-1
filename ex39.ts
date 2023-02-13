9f7e2296632fb94c7d5753513f5d2bc1e9791059
Rockey88L <abdullahrauf88@yahoo.com>
2023-02-13 23:01:11 +0500

    ex39

---
+++ ex39.ts
@@ -0,0 +1,10 @@
+//No.39
+// City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+
+function city_country(name, country) {
+  return console.log(`"${name}", "${country}"`);
+}
+
+city_country("Lahore", "Pakistan");
+city_country("Tokyo", "Japan");
+city_country("London", "England");
